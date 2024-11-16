8f8444c14f06d5f0a162766678676afa0fa2c254
proshantosaha <proshantoll71@gmail.com>
2024-11-16 16:06:34 +0600

    create login component and page ,add templete login

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -19,6 +19,7 @@ const Navbar = () => {
         </div>
 
         <ul className="flex gap-4 text-[#9C9C9C]">
+          <Link href="/login">Login</Link>
           <li>About</li>
           <li>Contact Us</li>
         </ul>
---
+++ components/auth/LoginForm.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React from "react";
+
+const LoginForm = () => {
+  const onSubmit = () => {};
+  return (
+    <form className="login-form" onSubmit={onSubmit}>
+      {/* <!-- email --> */}
+      <div>
+        <label htmlFor="email">Email Address</label>
+        <input type="email" name="email" id="email" />
+      </div>
+      {/* <!-- password --> */}
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+      </div>
+
+      <button
+        type="submit"
+        className="btn-primary w-full mt-4 bg-indigo-600 hover:bg-indigo-800"
+      >
+        Login
+      </button>
+    </form>
+  );
+};
+
+export default LoginForm;
