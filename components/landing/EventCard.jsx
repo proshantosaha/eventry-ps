8f9f8e551e95cf139554e7f07b2942a75b1368af
proshantosaha <proshantoll71@gmail.com>
2024-11-19 21:24:10 +0600

    added google search event schema

--- components/landing/EventCard.jsx
+++ components/landing/EventCard.jsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 import React from "react";
 import ActionButtons from "../ActionButtons";
 import Image from "next/image";
+import EventSchemaScript from "../meta/EventSchemaScript";
 
 const EventCard = ({ event }) => {
   return (
     <div className="overflow-hidden rounded-md bg-[#242526]">
+      <EventSchemaScript event={event} />
       <Image
         src={event.imageUrl}
         alt="Event 1"
