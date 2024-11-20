c19d1273f28f6d9535ad0322766c2d8a07505176
proshantosaha <proshantoll71@gmail.com>
2024-11-20 20:42:07 +0600

    suspense and loading added

--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
 import EventList from "@/components/landing/EventList";
 import Header from "@/components/landing/Header";
+import Loading from "@/components/loading";
 import Image from "next/image";
+import { Suspense } from "react";
 
 export default function Home({ searchParams: { query } }) {
   return (
     <section className="container">
       <Header />
-      <EventList query={query} />
+      <Suspense key={query} fallback={<Loading />}>
+        <EventList query={query} />
+      </Suspense>
     </section>
   );
 }
